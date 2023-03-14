6f78468e9ad51eb993a27dc6f78c6152b10b9d29
A89881 <abhay.sanatkumarmishra@elev.ga.ntig.se>
2023-03-14 21:10:58 +0100

    Finished Graphing and Deletes Duplicates

--- Files/main.py
+++ Files/main.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -7,32 +8,33 @@ class GraphingCalculator:
     def __init__(self, master):
         self.master = master
         master.title("Graphing Calculator")
+        self.plot_handles = {}  # keep track of plotted functions and their colors
 
         # Create function input label and entry box
-        self.function_label = tk.Label(master, text="Enter a function (in terms of x e.g., x + 1 or x**2): ")
+        self.function_label = tk.Label(master, text="function")
         self.function_label.pack()
         self.function_entry = tk.Entry(master)
         self.function_entry.pack()
 
         # Create x range input label and entry boxes
-        self.x_min_label = tk.Label(master, text="X-Min: ")
+        self.x_min_label = tk.Label(master, text="x-min: ")
         self.x_min_label.pack()
         self.x_min_entry = tk.Entry(master)
         self.x_min_entry.pack()
 
-        self.x_max_label = tk.Label(master, text="X-Max: ")
+        self.x_max_label = tk.Label(master, text="x-max: ")
         self.x_max_label.pack()
         self.x_max_entry = tk.Entry(master)
         self.x_max_entry.pack()
 
         # Create color input label and entry box
-        self.color_label = tk.Label(master, text="Color: ")
+        self.color_label = tk.Label(master, text="color: ")
         self.color_label.pack()
         self.color_entry = tk.Entry(master)
         self.color_entry.pack()
 
         # Create plot button
-        self.plot_button = tk.Button(master, text="Plot", command=self.plot_function)
+        self.plot_button = tk.Button(master, text="plot", command=self.plot_function)
         self.plot_button.pack()
 
     def plot_function(self):
@@ -43,24 +45,36 @@ class GraphingCalculator:
         x_range = (x_min, x_max)
         clr = str(self.color_entry.get())
 
-        # Define the x values to plot
-        num_points = abs(int(x_range[0])) + abs(int(x_range[1]))
-        print(num_points)
-        x_vals = np.linspace(x_range[0], x_range[1], num_points)
+        # Check if the function has already been plotted
+        if function_str in self.plot_handles:
+            # Update the color of the existing plot
+            handle = self.plot_handles[function_str]
+            handle.set_color(clr)
+            messagebox.showwarning(title="Warning", message="The function already exists")
+        else:
+            num_points = abs(int(x_range[0])) + abs(int(x_range[1]))
+            # Define the x values to plot
+            if num_points < 1000:
+                num_points = 1000
+            else:
+                num_points = abs(int(x_range[0])) + abs(int(x_range[1]))
+            x_vals = np.linspace(x_range[0], x_range[1], num_points)
 
-        # Create a namespace for the math functions
-        namespace = {'x': x_vals}
-        namespace.update(math.__dict__)
+            # Create a namespace for the math functions
+            namespace = {'x': x_vals}
+            namespace.update(math.__dict__)
 
-        # Evaluate the function for each x value
-        try:
-            y_vals = eval(function_str, namespace)
-        except:
-            print("Invalid function.")
-            return
+            # Evaluate the function for each x value
+            try:
+                y_vals = eval(function_str, namespace)
+            except:
+                messagebox.showerror(title="Error", message="Invalid function.")
+                print("Invalid function.")
+                return
 
-        # Create the plot
-        plt.plot(x_vals, y_vals, label=function_str, color=clr)
+            # Create the plot
+            handle, = plt.plot(x_vals, y_vals, label=function_str, color=clr)
+            self.plot_handles[function_str] = handle
 
         # Add labels and title
         plt.xlabel("X-Axis")
